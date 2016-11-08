8f1f1e5df8907f742b44afe815349fbb0f86f55a
SomnathCode <=>
2016-11-08 17:48:19 -0500

    Second Commit

---
+++ src/com/xyz/app/CameraComponent.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.xyz.app;
+
+/**
+ * GUI builder created Form
+ *
+ * @author jitendrasingh
+ */
+public class CameraComponent extends com.codename1.ui.Form {
+
+    public CameraComponent() {
+        this(com.codename1.ui.util.Resources.getGlobalResources());
+    }
+    
+    public CameraComponent(com.codename1.ui.util.Resources resourceObjectInstance) {
+        initGuiBuilderComponents(resourceObjectInstance);
+    }
+
+//-- DON'T EDIT BELOW THIS LINE!!!
+
+
+// <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initGuiBuilderComponents(com.codename1.ui.util.Resources resourceObjectInstance) {
+        setLayout(new com.codename1.ui.layouts.FlowLayout());
+        setTitle("CameraComponent");
+        setName("CameraComponent");
+    }// </editor-fold>
+
+//-- DON'T EDIT ABOVE THIS LINE!!!
+}
--- src/com/xyz/app/GuiComponent.java
+++ src/com/xyz/app/GuiComponent.java
@@ -5,6 +5,21 @@
  */
 package com.xyz.app;
 
+import com.codename1.capture.Capture;
+import com.codename1.io.Log;
+import com.codename1.ui.Dialog;
+import com.codename1.ui.Display;
+import com.codename1.ui.FontImage;
+import com.codename1.ui.Form;
+import com.codename1.ui.Graphics;
+import com.codename1.ui.Image;
+import com.codename1.ui.Label;
+import com.codename1.ui.Toolbar;
+import com.codename1.ui.layouts.BorderLayout;
+import com.codename1.ui.plaf.Style;
+import com.codename1.ui.plaf.UIManager;
+import java.io.IOException;
+
 /**
  * GUI builder created Form
  *
@@ -25,15 +40,20 @@ public class GuiComponent extends com.codename1.ui.Form {
     private com.codename1.ui.TextField gui_UserName = new com.codename1.ui.TextField();
     private com.codename1.ui.Label gui_Email = new com.codename1.ui.Label();
     private com.codename1.ui.TextField gui_email = new com.codename1.ui.TextField();
-    private com.codename1.ui.Container gui_Container_1 = new com.codename1.ui.Container(new com.codename1.ui.layouts.FlowLayout());
-    private com.codename1.ui.Button gui_Button_1 = new com.codename1.ui.Button();
+    private com.codename1.ui.Label gui_latitude = new com.codename1.ui.Label();
+    private com.codename1.ui.TextField gui_edit_latitude = new com.codename1.ui.TextField();
+    private com.codename1.ui.Label gui_label_longitude = new com.codename1.ui.Label();
+    private com.codename1.ui.TextField gui_edit_longitude = new com.codename1.ui.TextField();
+    private com.codename1.ui.Button gui_bttn_capture_pic = new com.codename1.ui.Button();
+    private com.codename1.components.SpanButton gui_Add = new com.codename1.components.SpanButton();
 
 
 // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void guiBuilderBindComponentListeners() {
         EventCallbackClass callback = new EventCallbackClass();
         gui_UserName.addActionListener(callback);
-        gui_Button_1.addActionListener(callback);
+        gui_bttn_capture_pic.addActionListener(callback);
+        gui_Add.addActionListener(callback);
     }
 
     class EventCallbackClass implements com.codename1.ui.events.ActionListener, com.codename1.ui.events.DataChangedListener {
@@ -54,8 +74,11 @@ public class GuiComponent extends com.codename1.ui.Form {
             if(sourceComponent == gui_UserName) {
                 onUserNameActionEvent(ev);
             }
-            if(sourceComponent == gui_Button_1) {
-                onButton_1ActionEvent(ev);
+            if(sourceComponent == gui_bttn_capture_pic) {
+                onbttn_capture_picActionEvent(ev);
+            }
+            if(sourceComponent == gui_Add) {
+                onAddActionEvent(ev);
             }
         }
 
@@ -71,22 +94,33 @@ public class GuiComponent extends com.codename1.ui.Form {
         addComponent(gui_UserName);
         addComponent(gui_Email);
         addComponent(gui_email);
-        addComponent(gui_Container_1);
-        gui_Container_1.setName("Container_1");
-        gui_Container_1.addComponent(gui_Button_1);
-        gui_Button_1.setText("Login");
-        gui_Button_1.setName("Button_1");
+        addComponent(gui_latitude);
+        addComponent(gui_edit_latitude);
+        addComponent(gui_label_longitude);
+        addComponent(gui_edit_longitude);
+        addComponent(gui_bttn_capture_pic);
+        addComponent(gui_Add);
         gui_Label_1.setText("UserName");
         gui_Label_1.setName("Label_1");
         gui_UserName.setHint("UserName");
-        gui_UserName.setText("");
         gui_UserName.setName("UserName");
         gui_Email.setText("Email");
         gui_Email.setName("Email");
         gui_email.setHint("Email Address");
-        gui_email.setText("");
         gui_email.setName("email");
-        gui_Container_1.setName("Container_1");
+        gui_latitude.setText("Latitude");
+        gui_latitude.setUIID("latitude");
+        gui_latitude.setName("latitude");
+        gui_edit_latitude.setHint("Latitude");
+        gui_edit_latitude.setName("edit_latitude");
+        gui_label_longitude.setText("Longitude");
+        gui_label_longitude.setName("label_longitude");
+        gui_edit_longitude.setHint("Longitude");
+        gui_edit_longitude.setName("edit_longitude");
+        gui_bttn_capture_pic.setText("Capture Picture");
+        gui_bttn_capture_pic.setName("bttn_capture_pic");
+        gui_Add.setText("Add Person");
+        gui_Add.setName("Add");
     }// </editor-fold>
 
 //-- DON'T EDIT ABOVE THIS LINE!!!
@@ -95,5 +129,48 @@ public class GuiComponent extends com.codename1.ui.Form {
 
     public void onButton_1ActionEvent(com.codename1.ui.events.ActionEvent ev) {
     }
+
+    public void onbttn_capture_picActionEvent(com.codename1.ui.events.ActionEvent ev) 
+    {
+        //Dialog.show("capture", "capture", "Ok", "Cancel");
+        capturePic();
+    }
+
+    public void onAddActionEvent(com.codename1.ui.events.ActionEvent ev) 
+    {
+        
+    }
+    
+    public void capturePic()
+    {
+        Toolbar.setGlobalToolbar(true);
+Form hi = new Form("Rounder", new BorderLayout());
+Label picture = new Label("", "Container");
+hi.add(BorderLayout.CENTER, picture);
+hi.getUnselectedStyle().setBgColor(0xff0000);
+hi.getUnselectedStyle().setBgTransparency(255);
+Style s = UIManager.getInstance().getComponentStyle("TitleCommand");
+Image camera = FontImage.createMaterial(FontImage.MATERIAL_CAMERA, s);
+hi.getToolbar().addCommandToRightBar("", camera, (ev) -> {
+    try {
+        int width = Display.getInstance().getDisplayWidth();
+        Image capturedImage = Image.createImage(Capture.capturePhoto(width, -1));
+        Image roundMask = Image.createImage(width, capturedImage.getHeight(), 0xff000000);
+        Graphics gr = roundMask.getGraphics();
+        gr.setColor(0xffffff);
+        gr.fillArc(0, 0, width, width, 0, 360);
+        Object mask = roundMask.createMask();
+        capturedImage = capturedImage.applyMask(mask);
+        picture.setIcon(capturedImage);
+        hi.revalidate();
+    } catch(IOException err) {
+        Log.e(err);
+    }
+});
+
+hi.show();
+
+    }
+
 
 }
